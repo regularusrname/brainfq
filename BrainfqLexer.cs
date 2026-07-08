5c4e60f1348e0993ea506231f71276da6d72971f
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-07-08 19:42:26 +0500

    add BrainfqLexer class

---
+++ BrainfqLexer.cs
@@ -0,0 +1,27 @@
+using System.Text;
+
+internal static class BrainfqLexer
+{
+    private static readonly HashSet<char> _lexPool = [ '.', ',', '<', '>', '+', '-', '[', ']' ];
+
+    internal static async Task<string> LexAnalyzeAsync(string rawText)
+    {
+        var rawTextLength = rawText.Length;
+
+        if (rawTextLength <= 0 || rawText is null)
+        {
+            throw new InvalidOperationException("Empty file: cannot parse");
+        }
+
+        var strBuilder = new StringBuilder();
+
+        for (int i = 0; i < rawTextLength; ++i)
+        {
+            if (_lexPool.Contains(rawText[i]))
+            {
+                strBuilder.Append(rawText[i]);
+            }
+        }
+        return strBuilder.ToString();
+    }
+}
